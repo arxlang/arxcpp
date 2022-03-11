30bdcbd1852d4d4ac7a8a6e64e87f939e0a047fb
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-11 00:23:00 -0400

    change git+ssh to https

--- scripts/prepare-conda-build.sh
+++ scripts/prepare-conda-build.sh
@@ -3,6 +3,9 @@
 GIT_ORIGIN=$(git config --get remote.origin.url)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+GIT_ORIGIN="`echo $GIT_ORIGIN | sed 's%git@%https://%g'`"
+GIT_ORIGIN="`echo $GIT_ORIGIN | sed 's%com:%com/%g'`"
+
 rm -rf /tmp/staged-recipes
 
 git clone --depth 1 https://github.com/conda-forge/staged-recipes.git /tmp/staged-recipes
@@ -14,5 +17,7 @@ cp -R conda/build /tmp/staged-recipes/recipes/arx
 sed -i "s/DOCKER_RUN_ARGS=\"-it\"/DOCKER_RUN_ARGS=\"\"/g" /tmp/staged-recipes/.scripts/run_docker_build.sh
 sed -i "s%{{git_url}}%${GIT_ORIGIN}%g" /tmp/staged-recipes/recipes/arx/meta.yaml
 sed -i "s:{{git_branch}}:${GIT_BRANCH}:g" /tmp/staged-recipes/recipes/arx/meta.yaml
+
+cat /tmp/staged-recipes/recipes/arx/meta.yaml
 
 cp conda/build/linux64.yaml /tmp/staged-recipes/.ci_support
