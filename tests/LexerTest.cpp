fe896ed7b322ff16440cf7a1a534f665b21eadf0
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-15 10:45:19 -0400

    Fix gtest usage

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -7,3 +7,8 @@ TEST(LexerTest, BasicAssertions) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
