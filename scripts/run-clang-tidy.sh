9ec9e6cd2feaf38f626584c3a2f6ba8ad6a24f6a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-31 21:23:34 -0400

    Fix clang-tidy call

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -3,9 +3,9 @@
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 
 clang-tidy \
-    --config-file ${PROJECT_PATH}/.clang-tidy \
+    --config-file="${PROJECT_PATH}/.clang-tidy" \
     ${PROJECT_PATH}/arx/**/*.cpp \
     ${PROJECT_PATH}/arx/**/*.h \
-    -p ${PROJECT_PATH}/build \
+    -p="${PROJECT_PATH}/build" \
     --header-filter="${CONDA_PREFIX}/include/llvm/**/*.h" \
     --extra-arg="-I${PROJECT_PATH}/arx/include"
