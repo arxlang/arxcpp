7e69ec769953c7fd2f6eb39bc20c3dcb9cff9b38
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-07-15 16:51:34 -0400

    Fix conda build script

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 set -ex
 
-CXX=clang++
-CC=clang
-
-LDFLAGS="$(llvm-config --ldflags) $LDFLAGS"
-CFLAGS="$(llvm-config --cflags) $CFLAGS"
-CXXFLAGS="$(llvm-config --cxxflags) $CXXFLAGS"
-CPPFLAGS="$(llvm-config --cppflags) $CPPFLAGS"
-
 export CXXFLAGS="`echo $CXXFLAGS | sed 's/-fno-exceptions//'`"
 
-mkdir -p ${SRC_DIR}/build/bin
-cd ${SRC_DIR}/build
+meson setup \
+  --prefix ${PREFIX} \
+  --libdir ${PREFIX}/lib \
+  --includedir ${PREFIX}/include \
+  --buildtype=release \
+  --native-file meson.native \
+  build .
+meson compile -C build
 
-cmake \
-    -GNinja \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=release \
-    -DCMAKE_C_COMPILER=${CC} \
-    -DCMAKE_CXX_COMPILER=${CXX} \
-    ..
-cmake --build .
-cmake --install . -v
-
-mv ${PREFIX}/build/bin/arx ${PREFIX}/bin/arx
+mv ${PREFIX}/build/arx ${PREFIX}/bin/arx
 chmod +x ${PREFIX}/bin/arx
 
 ctest
