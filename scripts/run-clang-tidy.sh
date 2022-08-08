48fb509b4ce5910a299918daf1f5dabfd7c25862
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-08-08 14:54:26 -0400

    Fix tidy issues

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -2,5 +2,12 @@
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 
-cd ${PROJECT_PATH}/build
-ninja --verbose tidy
+set -ex
+
+clang-tidy \
+    --config-file="${PROJECT_PATH}/.clang-tidy" \
+    --header-filter="${CONDA_PREFIX}/include" \
+    --extra-arg="-I${PROJECT_PATH}/arx/include" \
+    -p=${PROJECT_PATH}/artifacts \
+    ${PROJECT_PATH}/arx/**/*.cpp \
+    ${PROJECT_PATH}/arx/**/*.h
