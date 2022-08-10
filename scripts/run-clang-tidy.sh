2a008a2ffcd7870f092b08566ee45b74ca823646
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-08-10 16:13:03 -0400

    Transform compile_commands as tmpl file

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -4,10 +4,16 @@ PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. 
 
 set -ex
 
+mkdir -p /tmp/arx
+cp ${PROJECT_PATH}/artifacts/compile_commands.json.tmpl /tmp/arx/compile_commands.json
+
+sed -i s:\{\{PROJECT_PATH\}\}:${PROJECT_PATH}:g /tmp/arx/compile_commands.json
+sed -i s:\{\{CONDA_PREFIX\}\}:${CONDA_PREFIX}:g /tmp/arx/compile_commands.json
+
 clang-tidy \
     --config-file="${PROJECT_PATH}/.clang-tidy" \
     --header-filter="${CONDA_PREFIX}/include" \
     --extra-arg="-I${PROJECT_PATH}/arx/include" \
-    -p=${PROJECT_PATH}/artifacts \
+    -p=/tmp/arx \
     ${PROJECT_PATH}/arx/**/*.cpp \
     ${PROJECT_PATH}/arx/**/*.h
