a83dc7ee911f8dc6b278af5212b89b0a40019a7d
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-02-16 18:20:17 -0400

    chore(clang-tidy): Fix clang-tidy usage with meson

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -4,18 +4,18 @@ PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. 
 
 set -ex
 
-TMP_DIR=/tmp/arx
-mkdir -p ${TMP_DIR}
-cp ${PROJECT_PATH}/artifacts/compile_commands.json.tmpl ${TMP_DIR}/compile_commands.json
+# TMP_DIR=/tmp/arx
+# mkdir -p ${TMP_DIR}
+# cp ${PROJECT_PATH}/artifacts/compile_commands.json.tmpl ${TMP_DIR}/compile_commands.json
 
-sed -i s:\{\{PROJECT_PATH\}\}:${PROJECT_PATH}:g ${TMP_DIR}/compile_commands.json
-sed -i s:\{\{CONDA_PREFIX\}\}:${CONDA_PREFIX}:g ${TMP_DIR}/compile_commands.json
+# sed -i s:\{\{PROJECT_PATH\}\}:${PROJECT_PATH}:g ${TMP_DIR}/compile_commands.json
+# sed -i s:\{\{CONDA_PREFIX\}\}:${CONDA_PREFIX}:g ${TMP_DIR}/compile_commands.json
 
 clang-tidy \
     --config-file="${PROJECT_PATH}/.clang-tidy" \
     --header-filter="${CONDA_PREFIX}/include" \
     --extra-arg="-I${PROJECT_PATH}/src" \
-    -p="${TMP_DIR}" \
+    -p="${PROJECT_PATH}/build" \
     ${PROJECT_PATH}/src/*.cpp \
     ${PROJECT_PATH}/src/*.h \
     ${PROJECT_PATH}/src/codegen/*.cpp \
