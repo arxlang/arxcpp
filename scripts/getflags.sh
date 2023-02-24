1e7b498d77abdade6e5f6a01a9c9d52d399843e1
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-02-24 16:05:06 -0400

    Remove unnecessary files

--- scripts/getflags.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-llvm-config --cxxflags --ldflags --system-libs --libs core | sed 's/-fno-exceptions//g'
--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -4,13 +4,6 @@ PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. 
 
 set -ex
 
-# TMP_DIR=/tmp/arx
-# mkdir -p ${TMP_DIR}
-# cp ${PROJECT_PATH}/artifacts/compile_commands.json.tmpl ${TMP_DIR}/compile_commands.json
-
-# sed -i s:\{\{PROJECT_PATH\}\}:${PROJECT_PATH}:g ${TMP_DIR}/compile_commands.json
-# sed -i s:\{\{CONDA_PREFIX\}\}:${CONDA_PREFIX}:g ${TMP_DIR}/compile_commands.json
-
 clang-tidy \
     --config-file="${PROJECT_PATH}/.clang-tidy" \
     --header-filter="${CONDA_PREFIX}/include" \
